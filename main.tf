e07ec0f793072baaecd22fea12ff9a0fd2002060
Tomas <tomas.savukaitis@gmail.com>
2025-04-24 15:46:51 +0300

    web server that does not work

--- main.tf
+++ main.tf
@@ -3,16 +3,40 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-060a84cbcb5c14844"
   instance_type = "t2.micro"
+  associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.instance.id]
 
 user_data = <<-EOF
-              #!/bin/bash
-              yum install -y busybox
-              echo "Hello Tomas ;)" > index.html
-              nohup busybox httpd -f -p 8080 -h /home/ec2-user &
-              EOF
+  #!/bin/bash
+
+sudo wget https://busybox.net/downloads/binaries/1.28.1-defconfig-multiarch/busybox-x86_64
+sudo mv busybox-x86_64 busybox
+sudo chmod +x busybox
+sudo mv busybox /usr/local/bin/
+
+sudo yum install iptables -y
+sudo yum install iptables-services -y
+sudo systemctl start iptables
+sudo systemctl enable iptables
+
+sudo iptables -I INPUT -p tcp --dport 8080 -j ACCEPT        #kazkodel neveikia 
+sudo service iptables save
+
+
+
+# Create the index.html
+sudo echo "Hello Tomas ;)" > /home/ec2-user/index.html
+sudo chown ec2-user:ec2-user /home/ec2-user/index.html
+sudo chmod -R 755 /home/ec2-user
+
+# Start BusyBox HTTP server
+sudo nohup /usr/local/bin/busybox httpd -f -p 8080 -h /home/ec2-user 
+
+
+EOF
+
 
   tags = {
     Name = "Terraform-example"
@@ -24,18 +48,28 @@ user_data = <<-EOF
 
 
 
-
-
-
 resource "aws_security_group" "instance" {
-  
-
   ingress {
-    from_port       = 8080
-    to_port         = 8080
-    protocol        = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+ 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
 
 
