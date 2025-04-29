09cd034ee4acd49464bc3109861b23fb41eb5931
Tomas <tomas.savukaitis@gmail.com>
2025-04-29 09:51:36 +0300

    working load balancer

--- main.tf
+++ main.tf
@@ -88,10 +88,12 @@ data "aws_subnets" "default" {
 }
 
 resource "aws_autoscaling_group" "example" {
+  
+  max_size            = 5
+  min_size            = 2
+  
   vpc_zone_identifier = data.aws_subnets.default.ids
-
-  min_size = 2
-  max_size = 5
+  target_group_arns   = [aws_lb_target_group.asg.arn]
 
   launch_template {
     id      = aws_launch_template.example.id
@@ -100,10 +102,92 @@ resource "aws_autoscaling_group" "example" {
 
   tag {
     key                 = "Name"
-    value               = "terraform-asg-example"
+    value               = "asg-example-instance"
     propagate_at_launch = true
   }
 }
 
 
+resource "aws_lb" "example" {
+
+name               = "aws-asg-example"
+load_balancer_type = "application"
+subnets            = data.aws_subnets.default.ids 
+security_groups    = [aws_security_group.alb.id] 
+}
+
+
+resource "aws_lb_listener" "http" {
+   load_balancer_arn = aws_lb.example.arn
+   port              = 80
+   protocol          = "HTTP"
+
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+
   
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-exmaple-alb"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    
+    }
+  
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+  
+}
+
